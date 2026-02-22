ac66c0d5e30d76751a0307b84009b4fde93c79d1
Cassio Da Silva De Jesus <cassios@g.globo>
2026-02-22 15:59:17 -0300

    Refatorando o service

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from app.core.config import Config
+from app.core.extensions import db
+from app.routes.user_routes import bp as user_routes_bp
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+    
+    app.register_blueprint(user_routes_bp)
 
     return app
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,17 +1,14 @@
 from app.repositories.user_repository import UserRepository
-from app.model.user import User
 
 class UserService:
-    # __init__ criado para injetar a dependência do UserRepository, facilitando testes e manutenção
     def __init__(self, user_repository=None):
-        self.user_repository = user_repository or UserRepository()
+        self.user_repository = user_repository or UserRepository
 
     def create_user(self, name, email):
-        user = User(name=name, email=email)
-        return self.user_repository.add(user)
+        return self.user_repository.create_user(name, email)
 
     def get_all_users(self):
-        return self.user_repository.get_all()
+        return self.user_repository.get_all_users()
     
     def get_user_by_id(self, user_id):
-        return self.user_repository.get_by_id(user_id)
+        return self.user_repository.get_user_by_id(user_id)
